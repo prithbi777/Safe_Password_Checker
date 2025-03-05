a63d43dc815c538a8798dbc1132293e9c6cd259d
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2025-03-06 00:44:26 +0530

    Update Password-StrengthChecker!.cpp

--- Password-StrengthChecker!.cpp
+++ Password-StrengthChecker!.cpp
@@ -1,47 +1,56 @@
-#include <iostream>
-#include<stdlib.h>
-using namespace std;
-string n;
+/*
 
-int main()
-{  
-   string inputTarget;
-   cout<<"\n\n\n\t\t----Welcome To Password-Strength Checker----"<<"\n\n";
-   bool isfound = true;
-   int listRange;
-   for(int i=0;true;i++){
-      cin>>n;
-      if(i==0){
-         cout<<"Checking In Dictionary...\n";
-         listRange = stoi(n);
-         cout<<"Number Of Passwords In The Dictionary : "<<listRange<<endl;
-       if(listRange<10000000){
-       cout<<"Sorry! I Required Minimum No Of Passwords In The Dictoinary : NoP>=10000000\n";
-       cout<<"Please Update The Dictionary Where Minimum NoP>=10000000 Passwords Are Present!\n";
-       exit(0);
-      }
-       continue;  
-      }
-      if(i==1){
-         inputTarget = n;
-         continue;
-      }
-      if(n==inputTarget){
-         cout<<"Password Found In Given Dictionary : "<<"["<<n<<"]";
-         cout<<"\nAt Line Number "<<"["<<i+1<<"]";
-         cout<<" If all the passowords are spaceless!";
-         isfound = false;
-         cout<<"\nWarning : Change This Password Rightnow!\nThis Password Can Be Easily Cracked By Dictionary Attacks...";
-         break;
-      }
-      if(i>listRange+2){
-         break;
-      }
-      
-   }
-   if(isfound){
-      cout<<"Great : Password Isn't Present In The Dictionary!\nThat's Mean Your Password Is Very Tought To Crack By Dictionary Attacks...";
-   }
-   cout<<endl;
-   
+AUTHOR: Prithbiraj Mahanta
+DATA: 6 MARCH, 2025
+SUBJECT: Password Checker Tool
+LANGUAGES USED: C++
+
+
+
+*/
+
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout<<"\t\n\n###################### WELCOME TO PASSWORD CHECKER ######################\n"<<endl;
+    string filename = "w1.txt";  // FILE NAME
+    ifstream file(filename);     // OPEN FILE
+
+    if (!file) {  // CHECK THE VALIDITY OF THE FILE
+        cout << "Error: Unable to open " << filename << endl;
+        return 1;
+    }
+
+    string inputPassword;
+    cout << "Enter the password to check: ";
+    cin >> inputPassword;
+
+    string filePassword;
+    bool found = false;
+    int lineNumber = 0;
+
+    while (getline(file, filePassword)) { // IT READS EACH LINE
+        lineNumber++;
+        if (filePassword == inputPassword) {
+            found = true;
+            cout << "Warning: Password found in dictionary at line " << lineNumber << "!" << endl;
+            cout << "Change this password immediately! It is vulnerable to attacks.\n";
+            break;
+        }
+    }
+
+    file.close(); // BEST PRACTICE TO CLOSE THE FILE AFTER USING IT
+
+    if (!found) {
+        cout << "Great: Your password isn't in the dictionary!\n";
+        cout << "This means it's tougher to crack using dictionary attacks.\n";
+    }
+
+    return 0;
 }
