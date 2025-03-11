fb5f932bb2829aaf4ecec9fe627fdaa86b765960
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2025-03-11 15:31:07 +0530

    Update Password-StrengthChecker!.cpp

--- Password-StrengthChecker!.cpp
+++ Password-StrengthChecker!.cpp
@@ -2,7 +2,10 @@
 
 AUTHOR: Prithbiraj Mahanta
 DATA: 6 MARCH, 2025
-SUBJECT: Password Checker Tool
+SUBJECT: Password Checker Tool basically it prevents dictionary attacks. It takes a wordlist which may be used 
+in dictionary attacks for cracking files. How it works is suppose you thought a password to encrypt a file but who knows
+it might be cracked using dictionary attack so what you can do is before setting the password you can check whether 
+the password is already present in the dictionary or not so this is the purpose of this project.
 LANGUAGES USED: C++
 
 
